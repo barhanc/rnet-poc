feca1b4b91993fe5ccdb8d36cbbc8c1b0064ba33
Bartosz Hanc <bartosz.hanc02@gmail.com>
2026-06-01 02:13:14 +0200

    Delete cpp/core/Utils.h

--- cpp/core/Utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <jsi/jsi.h>
-
-namespace mylib::core::utils
-{
-    void install_getExecuTorchRegisteredBackends(facebook::jsi::Runtime &rt, facebook::jsi::Object &module);
-} // namespace mylib::core::utils
