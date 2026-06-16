39d34ef0a8dadca67fcdb1956e0f10aa54375bcb
Bartosz Hanc <bartosz.hanc02@gmail.com>
2026-06-16 14:07:07 +0200

    refactor: make llm runner attributes private

--- cpp/extensions/nlp/llm_runner.h
+++ cpp/extensions/nlp/llm_runner.h
@@ -11,15 +11,16 @@ namespace mylib::extensions::nlp::llm
     class LLMRunnerHostObject : public facebook::jsi::HostObject, public std::enable_shared_from_this<LLMRunnerHostObject>
     {
     public:
-        std::unique_ptr<executorch::extension::llm::TextLLMRunner> runner_;
-        std::mutex mutex_;
-        std::string modelPath_;
-        std::string tokenizerPath_;
-
         LLMRunnerHostObject(const std::string &modelPath, const std::string &tokenizerPath);
 
         facebook::jsi::Value get(facebook::jsi::Runtime &rt, const facebook::jsi::PropNameID &name) override;
         std::vector<facebook::jsi::PropNameID> getPropertyNames(facebook::jsi::Runtime &rt) override;
+
+    private:
+        std::unique_ptr<executorch::extension::llm::TextLLMRunner> runner_;
+        std::mutex mutex_;
+        std::string modelPath_;
+        std::string tokenizerPath_;
     };
 
     void install_createLLMRunner(facebook::jsi::Runtime &rt, facebook::jsi::Object &module);
